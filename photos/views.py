5280bc7d5267290c519223385a0bb54006ae437a
IvyJeptoo <ivy.jeptoo@student.moringaschool.com>
2022-05-28 00:40:07 +0300

    add modals

--- photos/views.py
+++ photos/views.py
@@ -9,8 +9,8 @@ def gallery(request):
     return render(request,'photos/gallery.html', context)
 
 def viewPhoto(request, pk):
-    
-    return render(request,'photos/photo.html')
+    photo = Photo.objects.get(id=pk)
+    return render(request,'photos/photo.html',{'photo':photo})
 
 def addPhoto(request):
     
