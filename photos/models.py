b1d89594b7ddde112c999aef5ba0903fcce77f91
IvyJeptoo <ivy.jeptoo@student.moringaschool.com>
2022-05-29 01:46:54 +0300

    create migrations

--- photos/models.py
+++ photos/models.py
@@ -7,16 +7,19 @@ class Category(models.Model):
     def __str__(self):
         return self.name
     
+class Location(models.Model):
+    location = models.CharField(max_length=100, null=False,blank=False)
+    
+    def __str__(self):
+        return self.location
+    
 class Photo(models.Model):
     category = models.ForeignKey(Category,on_delete=models.SET_NULL, null=True, blank=True)
+    location = models.ForeignKey(Location,on_delete=models.SET_NULL, null=False, blank=False)
     image = models.ImageField(null=False,blank=False)
     description = models.TextField()
     
     def __str__(self):
         return self.description
     
-class Location(models.Model):
-    location = models.CharField(max_length=100, null=False,blank=False)
-    
-    def __str__(self):
-        return self.location
+
