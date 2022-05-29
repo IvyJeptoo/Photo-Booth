cd314e5d0d0dfe2a8685f2ae220049a00d5f5447
IvyJeptoo <ivy.jeptoo@student.moringaschool.com>
2022-05-29 05:13:19 +0300

    register locations url

--- photos/urls.py
+++ photos/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('',views.gallery,name='gallery'),
+    path('location/',views.location,name='location'),
     path('photo/<str:pk>add/',views.viewPhoto,name='photo'),
     path('add/',views.addPhoto,name='add'),
 ]
